b1b4d5ca522e299113483b70e0d0cb71445f4ddd
coderk3 <kunalkumar.kus@gmail.com>
2018-05-28 02:23:01 +0530

    post deatils added

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
-
 def post_list(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})
